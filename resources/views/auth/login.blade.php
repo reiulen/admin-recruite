5640c4e64e8229b6a0940efb5ab8ba0341d6fbf6
reiulen <reiandika10@gmail.com>
2023-06-15 03:45:16 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Login | APLIKASI LAPORAN HAWASBID PENGADILAN AGAMA CIREBON</title>
+    <title>Login</title>
     <link rel="icon" type="image/png" href="/assets/gambar/logo.png" />
 
     <!-- Google Font: Source Sans Pro -->
